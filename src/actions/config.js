46dffdd982d4c7ad541878703ce0a565d995ba46
reaksmey1 <chea.reaksmey@gmail.com>
2018-02-13 10:46:31 +1300

    move to production

--- src/actions/config.js
+++ src/actions/config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'https://s.hakatours.com';
-export const BASE_URL = 'http://127.0.0.1:3000';
+// export const BASE_URL = 'http://127.0.0.1:3000';
+export const BASE_URL = 'https://hakatours.com';
